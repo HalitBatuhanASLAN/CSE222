29ea17b51e166eef7f17c73cfa623985018748e8
Halit Batuhan ASLAN <halitbatuhanaslan@gmail.com>
2025-05-08 13:51:28 +0300

    works but more than 10second

--- Hw6HashSetHashMap/src/SpellChecker/SpellChecker.java
+++ Hw6HashSetHashMap/src/SpellChecker/SpellChecker.java
@@ -62,14 +62,19 @@ public class SpellChecker
         System.out.println("distance 1 variants;*********");
         variant(input, variants);
         System.out.println("distance 2 variants;*********");
-        //generateEditDistance2(variants);
+        generateEditDistance2(variants);
         return variants;
     }
 
     private static void generateEditDistance2(GTUArrayList<String> variants)
     {
-        for(String input : variants)
+        GTUArrayList<String> previous = variants;
+        int prevSize = previous.size();
+        for(int i = 0;i<prevSize;i++)
+        {
+            String input = previous.get(i);
             variant(input, variants);
+        }
     }
 
 
@@ -128,97 +133,3 @@ public class SpellChecker
         return false;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-    /*
-    private static GTUHashSet<String> generateEditDistance1(String input)
-    {
-        GTUHashSet<String> variants = new GTUHashSet<>();
-        variant(input, variants);
-        return variants;
-    }
-
-    private static GTUHashSet<String> generateEditDistance2(String input)
-    {
-        GTUHashSet<String> distance1Variants = generateEditDistance1(input);
-        GTUHashSet<String> distance2Variants = new GTUHashSet<>();
-
-        for (String variant : distance1Variants)
-        {
-            GTUHashSet<String> newVariants = generateEditDistance1(variant);
-            distance2Variants.addAll(newVariants);
-
-            // Performansı kontrol altında tutmak için bir sınır koyabilirsiniz
-            if (distance2Variants.size() > 10000) {
-                System.out.println("Too many variants, stopping further generation.");
-                break;
-            }
-        }
-        return distance2Variants;
-    }
-
-    private static void variant(String input,GTUHashSet<String> variants)
-    {
-        //for distance 1 character variants
-        for(int i = 0;i<=input.length();i++)
-        {
-            //adding character
-            for(char ch = 'a';ch <= 'z';ch++)
-            {
-                String addedVairant = input.substring(0,i) + ch + input.substring(i);
-                //System.out.println(addedVairant);
-                if(!containsChecker(addedVairant,variants))
-                    variants.add(addedVairant);
-            }
-        }
-        
-        for(int i = 0;i<input.length();i++)
-        {
-            //deleting character
-            String deletedVariant = input.substring(0,i) + input.substring(i+1);
-            //System.out.println(deletedVariant);
-            if(!containsChecker(deletedVariant,variants))
-                variants.add(deletedVariant);
-
-            //changing character
-            for(char ch = 'a';ch <= 'z';ch++)
-            {
-                String changedVariant = input.substring(0,i) + ch + input.substring(i+1);
-                //System.out.println(changedVariant);
-                if(!containsChecker(changedVariant,variants))
-                    variants.add(changedVariant);
-            }
-
-            //changing positions
-            for(int j = i+1;j<input.length();j++)
-            {
-                char tmp = input.charAt(j);
-                char tmp2 = input.charAt(i);
-                String newVariant = input.substring(0, i) + tmp + input.substring(i+1, j) + tmp2 + input.substring(j+1);
-                //System.out.println(newVariant);
-                if(!containsChecker(newVariant,variants))
-                    variants.add(newVariant);
-            }
-        }
-
-    }
-    private static boolean containsChecker(String input,GTUHashSet<String> variants)
-    {
-        for (int i = 0; i < variants.size(); i++) {
-            if (input.equals(variants.get(i))) {
-                return true;
-            }
-        }
-        return false;
-    }*/
